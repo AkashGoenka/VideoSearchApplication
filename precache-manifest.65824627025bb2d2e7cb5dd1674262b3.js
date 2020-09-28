ae1d32c3cbeaa452944dde8f21bc6f03e668ad05
Akash <akash.goenka@rocketmail.com>
2020-09-28 12:51:26 +0530

    First Commit

---
+++ precache-manifest.65824627025bb2d2e7cb5dd1674262b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d659921c46f9304a8f8a41ef66d3f6",
+    "url": "/VideoSearchApplication/index.html"
+  },
+  {
+    "revision": "198c5657e2b414a08e75",
+    "url": "/VideoSearchApplication/static/css/main.16dee05f.chunk.css"
+  },
+  {
+    "revision": "38cd1e6c455add05b4eb",
+    "url": "/VideoSearchApplication/static/js/2.85c45618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoSearchApplication/static/js/2.85c45618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198c5657e2b414a08e75",
+    "url": "/VideoSearchApplication/static/js/main.8b9bb311.chunk.js"
+  },
+  {
+    "revision": "88300e3539d4b1a0c169",
+    "url": "/VideoSearchApplication/static/js/runtime-main.db223253.js"
+  }
+]);
